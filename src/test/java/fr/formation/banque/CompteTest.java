8ddd2ee8c5dbea84aea99f8ac2d755c9861a8ca2
SLIMANI REDOUAN <red1.slimani@gmail.com>
2021-11-04 16:21:42 +0100

    ajout des tests selenium NavigationTest

--- src/test/java/fr/formation/banque/CompteTest.java
+++ src/test/java/fr/formation/banque/CompteTest.java
@@ -7,5 +7,40 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CompteTest {
+	
+	//Référence de l'objet a tester
+	private Compte compte;
+	
+	@Before
+	public void setUp() {
+		compte = new Compte(123, 2000.00, "redouan"); 
+	}
+	
+	@Test
+	public void testGetNumero() {
+		long numero = compte.getNumero();
+		assertEquals(123, numero);
+	}
+	
+	@Test
+	public void testGetReferenceClient() {
+		String referenceClient = compte.getReferenceClient();
+		assertEquals("redouan", referenceClient);
+	}
+	
+	@Test
+	public void testSetNumero() {
+		compte.setNumero(4764);
+		long numero = compte.getNumero();
+		assertEquals(4764, numero);
+	}
+	
+	@Test
+	public void testSetReferenceClient() {
+		compte.setReferenceClient("slimani");
+		String referenceClient = compte.getReferenceClient();
+		assertEquals("slimani", referenceClient);
+	}
+	
 
 }
